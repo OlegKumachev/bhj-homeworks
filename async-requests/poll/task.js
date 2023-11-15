b4ed0e7bf4806ad80847dd69f8f165b158070742
Oleg Kumachev <kumachev.oleg@gmail.com>
2023-11-15 19:44:16 +0300

    first commit poll

---
+++ async-requests/poll/task.js
@@ -0,0 +1,28 @@
+const xhr = new XMLHttpRequest();
+const pollTitle = document.getElementById('poll__title');
+const pollAnswersContainer = document.getElementById('poll__answers');
+
+xhr.addEventListener('readystatechange', () => {
+    if (xhr.readyState === xhr.DONE) {
+        const dataJson = JSON.parse(xhr.responseText);
+        const question = dataJson.data.title;
+        const answers = dataJson.data.answers;
+
+        pollTitle.textContent = question;
+        pollAnswersContainer.innerHTML = '';
+
+        answers.forEach(answer => {
+            const button = document.createElement('button');
+            button.className = 'poll__answer';
+            button.textContent = answer;
+            pollAnswersContainer.appendChild(button);
+
+            button.addEventListener('click', () => {
+                alert(`Спасибо, ваш голос зачитан!`);
+            });
+        });
+    }
+});
+
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
+xhr.send();
